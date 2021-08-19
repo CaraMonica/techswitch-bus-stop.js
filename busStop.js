71541c4077620b27dec59e9b8b61d85ed9e7cbb2
caramonica <caramonica@hotmail.co.uk>
2021-08-19 16:01:57 +0100

    retreives bus stop info from postcode

--- busStop.js
+++ busStop.js
@@ -1,19 +1,50 @@
-const fetch = require("node-fetch");
 const TECHSWITCH_STOP = "490008660N";
-const TFL_KEY = "?app_key=4270c89bdf034e6686245442ece733f5";
+const IMPERIAL_POSTCODE = "SW72AZ";
+const POSTCODES_API = "http://api.postcodes.io/postcodes/";
 
-const stopArrivalsURL = `https://api.tfl.gov.uk/StopPoint/${TECHSWITCH_STOP}/Arrivals`;
+const fetch = require("node-fetch");
+
+const getStopArrivalsAPI = (stopID) =>
+  `https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals`;
+
+const getStopsInAreaAPI = (lat, lon, radius) =>
+  `https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}&stopTypes=NaptanPublicBusCoachTram&radius=${radius}`;
 
 const logNextNBuses = (buses, n) =>
   buses
     .sort((a, b) => a.timeToStation - b.timeToStation)
     .slice(0, n)
-    .map(({ lineName, timeToStation }) => ({
+    .map(({ lineName, destinationName, timeToStation }) => ({
       lineName,
+      destinationName,
       timeToStation,
     }))
-    .forEach(console.log);
+    .forEach((element) => {
+      element["timeToStation"] = `${Math.floor(
+        element["timeToStation"] / 60
+      )} min: ${Math.floor(element["timeToStation"] % 60)}s`;
+      console.log(element);
+    });
 
-fetch(stopArrivalsURL)
+fetch(POSTCODES_API + IMPERIAL_POSTCODE)
+  .then((response) => response.json())
+  .then((json) =>
+    fetch(getStopsInAreaAPI(json.result.latitude, json.result.longitude, 1000))
+  )
+  .then((response) => response.json())
+  .then((json) =>
+    json.stopPoints
+      .sort((a, b) => a.distance - b.distance)
+      .slice(0, 2)
+      .map((element) => element.naptanId)
+  )
+  .then((stops) => {
+      console.log(stops[0])
+      fetch(getStopArrivalsAPI(stops[0]))})
   .then((response) => response.json())
   .then((json) => logNextNBuses(json, 5));
+
+
+// fetch(getStopArrivalsAPI(TECHSWITCH_STOP))
+//   .then((response) => response.json())
+//   .then((json) => logNextNBuses(json, 5));
