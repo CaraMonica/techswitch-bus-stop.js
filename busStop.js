f543858468940e39d19a4431ff245d0e2d9012d1
caramonica <caramonica@hotmail.co.uk>
2021-08-19 13:46:54 +0100

    function to find soonest N arrivals to a stop

--- busStop.js
+++ busStop.js
@@ -4,6 +4,16 @@ const TFL_KEY = "?app_key=4270c89bdf034e6686245442ece733f5";
 
 const stopArrivalsURL = `https://api.tfl.gov.uk/StopPoint/${TECHSWITCH_STOP}/Arrivals`;
 
+const logNextNBuses = (buses, n) =>
+  buses
+    .sort((a, b) => a.timeToStation - b.timeToStation)
+    .slice(0, n)
+    .map(({ lineName, timeToStation }) => ({
+      lineName,
+      timeToStation,
+    }))
+    .forEach(console.log);
+
 fetch(stopArrivalsURL)
   .then((response) => response.json())
-  .then((body) => console.log(body));
+  .then((json) => logNextNBuses(json, 5));
