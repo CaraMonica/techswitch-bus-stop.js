fb15908b259ed8749e3a69002255b770a4288bce
caramonica <caramonica@hotmail.co.uk>
2021-08-20 11:33:35 +0100

    initial user input checks

--- busStop.js
+++ busStop.js
@@ -3,7 +3,8 @@ const IMPERIAL_POSTCODE = "SW72AZ";
 const EUSTON_POSTCODE = "NW12RT";
 const POSTCODES_API = "http://api.postcodes.io/postcodes/";
 const NUM_BUSES = 5;
-const SEARCH_RADIUS = 500;
+const MIN_RADIUS = 100;
+const MAX_RADIUS = 2000;
 
 const readline = require("readline-sync");
 const fetch = require("node-fetch");
@@ -15,7 +16,7 @@ const getStopsInAreaAPI = (lat, lon, radius) =>
   `https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}&stopTypes=NaptanPublicBusCoachTram&radius=${radius}`;
 
 const convertSecsToMins = (seconds) =>
-  `${Math.floor(seconds / 60)} min and ${seconds % 60} s`;
+  `${Math.floor(seconds / 60)} minutes, ${seconds % 60} seconds`;
 
 const getUserInput = (message) => {
   console.log(message);
@@ -29,50 +30,71 @@ const logNextNBuses = (buses, n) =>
     .map(({ lineName, destinationName, timeToStation }) => ({
       lineName,
       destinationName,
-      timeToStation,
+      timeToStation: convertSecsToMins(timeToStation),
     }))
     .forEach((element) => {
-      element.timeToStation = convertSecsToMins(element.timeToStation);
-      console.log(`The ${element.lineName} to ${element.destinationName} in ${element.timeToStation}`);
+      console.log(
+        `The ${element.lineName} to ${element.destinationName} in ${element.timeToStation}`
+      );
     });
 
-const postcode = getUserInput("\nEnter Postcode with no spaces:");
+const isValidPostcode = (code) => {
+  const postcodeRegEx = /[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}/;
+  return postcodeRegEx.test(code);
+};
+
+let postcode;
+
+while (true) {
+  try {
+    postcode = getUserInput("Enter postcode in London:");
+    if (isValidPostcode(postcode)) break;
+    else throw "Not a valid postcode.";
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+let radius;
+while (true) {
+  try {
+    radius = parseInt(
+      getUserInput(`\nEnter radius to search in meters (${MIN_RADIUS}-${MAX_RADIUS}):`)
+    );
+    if (isNaN(radius)) throw "not a number";
+    else if (radius < MIN_RADIUS) throw `radius must be greater than ${MIN_RADIUS}`;
+    else if (radius > MAX_RADIUS) throw `radius must be less than ${MAX_RADIUS}`;
+    else break;
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 fetch(POSTCODES_API + postcode)
   .then((response) => response.json())
   .then((json) =>
     fetch(
-      getStopsInAreaAPI(
-        json.result.latitude,
-        json.result.longitude,
-        SEARCH_RADIUS
-      )
+      getStopsInAreaAPI(json.result.latitude, json.result.longitude, radius)
     )
   )
   .then((response) => response.json())
-  .then((json) => {
-    const stops = json.stopPoints
+  .then((json) =>
+    json.stopPoints
       .sort((a, b) => a.distance - b.distance)
       .slice(0, 2)
       .map(({ naptanId, indicator, distance }) => ({
         naptanId,
         indicator,
-        distance,
-      }));
-    stops.forEach(
-      (element) => (element.distance = Math.floor(element.distance))
-    );
-    return stops;
-  })
-  .then((stops) => {
-    console.log(`\n${stops[0].indicator}, distance ${stops[0].distance} m`);
-    fetch(getStopArrivalsAPI(stops[0].naptanId))
-      .then((response) => response.json())
-      .then((json) => logNextNBuses(json, NUM_BUSES))
-      .then(() => {
-        console.log(`\n${stops[1].indicator}, distance ${stops[1].distance} m`);
-        fetch(getStopArrivalsAPI(stops[1].naptanId))
-          .then((response) => response.json())
-          .then((json) => logNextNBuses(json, NUM_BUSES));
-      });
-  });
+        distance: Math.floor(distance),
+      }))
+  )
+  .then((stops) =>
+    stops.forEach((stop) =>
+      fetch(getStopArrivalsAPI(stop.naptanId))
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(`\n${stop.indicator}, distance ${stop.distance} m`);
+          logNextNBuses(json, NUM_BUSES);
+        })
+    )
+  );
